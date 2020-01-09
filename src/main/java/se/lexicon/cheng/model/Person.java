82fb0ff14e8568bc72c7ec9e98a74f167f880561
Cheng Tao <cheng.tao86@gmail.com>
2020-01-09 13:33:24 +0100

    Person Class added

--- src/main/java/se/lexicon/cheng/model/Person.java
+++ src/main/java/se/lexicon/cheng/model/Person.java
@@ -1,7 +1,33 @@
 package se.lexicon.cheng.model;
 
 public class Person{
+    private final int personId;
+    private String firstName;
+    private String lastName;
 
+    public Person(int personId, String firstName, String lastName) {
+        this.personId = personId;
+        setFirstName(firstName);
+        setLastName(lastName);
+    }
 
+    public int getPersonId() {
+        return personId;
+    }
 
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 }
---
+++ src/test/java/se/lexicon/cheng/model/PersonTest.java
@@ -0,0 +1,20 @@
+package se.lexicon.cheng.model;
+
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class PersonTest {
+    @Test
+    public void given_id_firstName_and_lastName_successfully_create_object(){
+        String firstName = "Test";
+        String lastName = "Testsson";
+        int personId = 1;
+
+        Person testObject = new Person(personId, firstName, lastName);
+
+        assertEquals(personId, testObject.getPersonId());
+        assertEquals(firstName, testObject.getFirstName());
+        assertEquals(lastName, testObject.getLastName());
+    }
+}
